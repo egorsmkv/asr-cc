272165ba93785ebeaef9ba868548b68290abb3e7
Yehor Smoliakov <egorsmkv@gmail.com>
2024-02-15 21:24:39 +0000

    Add an empty state

--- apps/frontend/resources/views/projects/show.blade.php
+++ apps/frontend/resources/views/projects/show.blade.php
@@ -20,4 +20,8 @@
 
     <h2 class="text-4xl font-thin text-gray-900 dark:text-white">Videos</h2>
 
+    <div class="mt-2 p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+        No videos yet.
+    </div>
+
 @endsection
